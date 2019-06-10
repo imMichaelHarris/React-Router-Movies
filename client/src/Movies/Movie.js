2cdda12fa3d926cca1b4f72809567f637d13a23d
Michael Harris <mhguitar1997@gmail.com>
2019-06-10 14:48:12 -0700

    Refactored to be DRY

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
+import MovieCard from "./MovieCard";
 
 export default class Movie extends Component {
   constructor(props) {
@@ -24,7 +25,6 @@ export default class Movie extends Component {
       .catch(error => {
         console.error(error);
       });
-
   };
   // Uncomment this code when you're ready for the stretch problems
   // componentWillReceiveProps(newProps){
@@ -43,25 +43,9 @@ export default class Movie extends Component {
       return <div>Loading movie information...</div>;
     }
 
-    const { title, director, metascore, stars } = this.state.movie;
     return (
       <div className="save-wrapper">
-        <div className="movie-card">
-          <h2>{title}</h2>
-          <div className="movie-director">
-            Director: <em>{director}</em>
-          </div>
-          <div className="movie-metascore">
-            Metascore: <strong>{metascore}</strong>
-          </div>
-          <h3>Actors</h3>
-
-          {stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>
-          ))}
-        </div>
+        <MovieCard movie={this.state.movie} />
         <div className="save-button">Save</div>
       </div>
     );
